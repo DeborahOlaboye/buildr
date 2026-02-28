6a447b002e62bcde980a71b15a507fc736ec8663
DeborahOlaboye <deboraholaboye@gmail.com>
2026-02-28 07:48:47 +0100

    chore(tailwind): expand content paths for new lib directory

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,9 +3,11 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   darkMode: ["class"],
   content: [
+    // include new convention file structure for Next.js 15.2
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./lib/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     container: {
